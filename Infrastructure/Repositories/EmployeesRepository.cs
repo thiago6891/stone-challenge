cab2e0b704527d034bd07a0029a805b355187020
Thiago Cordeiro <thiago6891@gmail.com>
2019-04-23 21:15:58 -0300

    Set up Employees repository to use the default DB context.

--- Infrastructure/Repositories/EmployeesRepository.cs
+++ Infrastructure/Repositories/EmployeesRepository.cs
@@ -1,24 +1,22 @@
-﻿using ApplicationCore.Enumerations;
-using ApplicationCore.Interfaces;
+﻿using ApplicationCore.Interfaces;
 using ApplicationCore.Models;
-using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Infrastructure.Repositories
 {
     public class EmployeesRepository : IEmployeesRepository
     {
+        private readonly DefaultContext _db;
+
+        public EmployeesRepository(DefaultContext db)
+        {
+            _db = db;
+        }
+
         public IEnumerable<Employee> GetEmployees()
         {
-            // TODO: replace this with real code
-            var employees = new List<Employee>
-            {
-                new Employee(9968, "Victor Wilson", Sector.Directors, "Diretor Financeiro", 12696.20M, new DateTime(2012, 1, 5), false),
-                new Employee(4468, "Flossie Wilson", Sector.Accounting, "Auxiliar de Contabilidade", 1396.52M, new DateTime(2015, 1, 5), false),
-                new Employee(8174, "Sherman Hodges", Sector.CustomerRelationship, "Líder de Relacionamento", 3899.74M, new DateTime(2015, 6, 7), false)
-            };
-
-            return employees;
+            return _db.Employees.ToList();
         }
     }
 }
