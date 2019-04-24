81a4d6e2f2b18d3969014b119449b3ff000d4dda
Thiago Cordeiro <thiago6891@gmail.com>
2019-04-23 21:15:09 -0300

    Add DB Context and an entity configuration class.

---
+++ Infrastructure/DefaultContext.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationCore.Enumerations;
+using ApplicationCore.Models;
+using Infrastructure.EntityConfigurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Infrastructure
+{
+    public class DefaultContext : DbContext
+    {
+        public DefaultContext(DbContextOptions<DefaultContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new EmployeeEntityConfiguration());
+        }
+    }
+}
---
+++ Infrastructure/EntityConfigurations/EmployeeEntityConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using ApplicationCore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.EntityConfigurations
+{
+    public class EmployeeEntityConfiguration : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.HasKey(e => e.RegistrationNumber);
+
+            builder.Property(e => e.RegistrationNumber)
+                .UsePropertyAccessMode(PropertyAccessMode.Field)
+                .IsRequired();
+
+            builder.Property(e => e.Name)
+                .UsePropertyAccessMode(PropertyAccessMode.Field)
+                .IsRequired();
+
+            builder.Property(e => e.Sector)
+                .UsePropertyAccessMode(PropertyAccessMode.Field)
+                .IsRequired();
+
+            builder.Property(e => e.JobTitle)
+                .UsePropertyAccessMode(PropertyAccessMode.Field)
+                .IsRequired();
+
+            builder.Property(e => e.GrossSalary)
+                .UsePropertyAccessMode(PropertyAccessMode.Field)
+                .IsRequired()
+                .HasColumnType("Money");
+
+            builder.Property(e => e.AdmissionDate)
+                .UsePropertyAccessMode(PropertyAccessMode.Field)
+                .IsRequired();
+
+            builder.Property(e => e.IsIntern)
+                .UsePropertyAccessMode(PropertyAccessMode.Field)
+                .IsRequired();
+
+            builder.Ignore(e => e.Bonus);
+        }
+    }
+}
