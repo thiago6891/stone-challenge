930ee868e4e8d791df178d8d50428f48be39de05
Thiago Cordeiro <thiago6891@gmail.com>
2019-04-23 21:18:08 -0300

    Add initial migration.

---
+++ Infrastructure/Migrations/20190423231243_InitialMigration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    RegistrationNumber = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    Sector = table.Column<int>(nullable: false),
+                    JobTitle = table.Column<string>(nullable: false),
+                    GrossSalary = table.Column<decimal>(type: "Money", nullable: false),
+                    AdmissionDate = table.Column<DateTime>(nullable: false),
+                    IsIntern = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.RegistrationNumber);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
